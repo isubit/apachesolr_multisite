697c340852e85743ee4b4eec4610f9b780c960db
Nick Booher <njbooher@gmail.com>
2016-03-08 12:55:51 -0600

    Moved metadata generation and insertion to a separate module

--- index/apachesolr_multisiteindex.api.php
+++ index/apachesolr_multisiteindex.api.php
@@ -3,5 +3,5 @@
 /**
  * Alter the site meta-data before it's sent to the Solr index.
  */
-function hook_apachesolr_multisitesearch_metadata_alter($document) {
+function hook_apachesolr_multisiteindex_metadata_alter($document) {
 }
